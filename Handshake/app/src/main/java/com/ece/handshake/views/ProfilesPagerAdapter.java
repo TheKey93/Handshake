15510e1377ad87aadc2e38865cf569f9105a5962
Zeki Sherif <zeki.sherif@gmail.com>
2015-07-12 00:41:36 -0400

    WIP Profiles viewpager/tabs

---
+++ Handshake/app/src/main/java/com/ece/handshake/views/ProfilesPagerAdapter.java
@@ -0,0 +1,34 @@
+package com.ece.handshake.views;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+
+public class ProfilesPagerAdapter extends FragmentStatePagerAdapter {
+    private int mNumOfTabs;
+
+    public ProfilesPagerAdapter(FragmentManager fm, int numOfTabs) {
+        super(fm);
+        this.mNumOfTabs = numOfTabs;
+    }
+    @Override
+    public Fragment getItem(int position) {
+        switch(position) {
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            default:
+                return null;
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
+}
